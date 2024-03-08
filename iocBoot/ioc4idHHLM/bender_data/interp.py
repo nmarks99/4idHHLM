97911d813aa09a879428e7c8463c5fe1dd5bed60
nmarks99 <nmarks@anl.gov>
2024-03-08 11:03:13 -0600

    minor adjustements to interp.py

--- iocBoot/ioc4idHHLM/bender_data/interp.py
+++ iocBoot/ioc4idHHLM/bender_data/interp.py
@@ -8,11 +8,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.interpolate import interp1d
 
-SAVE = False
+# set to True to save interpolated data to CSV
+SAVE = False 
 
+# load the raw data and plot it
 data = np.loadtxt("./bender.csv", delimiter=",")
 index = data[:,0]
-
 fig0, ax0 = plt.subplots()
 ax0.plot(data[:,1][0:7], data[:,3][0:7], "bo")
 ax0.plot(data[:,1][8:], data[:,3][8:], "bo")
@@ -27,7 +28,6 @@ roc_concave = data[:,3][0:7]
 f_concave = interp1d(ucounts_concave, roc_concave, kind="cubic")
 x_fit_concave = np.linspace(ucounts_concave[0], ucounts_concave[-1], 100)
 y_fit_concave = [f_concave(xi) for xi in x_fit_concave]
-
 fig1, ax1 = plt.subplots()
 ax1.plot(ucounts_concave, roc_concave, "bo", label="measured")
 ax1.plot(x_fit_concave, y_fit_concave, "r.", label="interpolated")
@@ -45,11 +45,6 @@ roc_convex = data[:,3][8:]
 f_convex = interp1d(ucounts_convex, roc_convex, kind="cubic")
 x_fit_convex = np.linspace(ucounts_convex[0], ucounts_convex[-1], 100)
 y_fit_convex = [f_convex(xi) for xi in x_fit_convex]
-
-if SAVE:
-    np.savetxt("concave.csv", np.column_stack([x_fit_concave, y_fit_concave]), delimiter=",")
-    np.savetxt("convex.csv", np.column_stack([x_fit_convex, y_fit_convex]), delimiter=",")
-
 fig2, ax2 = plt.subplots()
 ax2.plot(ucounts_convex, roc_convex, "bo", label="measured")
 ax2.plot(x_fit_convex, y_fit_convex, "r.", label="interpolated")
@@ -60,5 +55,11 @@ ax2.set_yticks(np.arange(-100,30,10))
 ax2.legend()
 ax2.grid()
 
+# save the interpolated data to CSV files
+if SAVE:
+    np.savetxt("concave.csv", np.column_stack([x_fit_concave, y_fit_concave]), delimiter=",")
+    np.savetxt("convex.csv", np.column_stack([x_fit_convex, y_fit_convex]), delimiter=",")
+
+# show the plots
 plt.show()
 
