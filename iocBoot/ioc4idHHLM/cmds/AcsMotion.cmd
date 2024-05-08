3c10ae0b7dc7e8222f32a56c5d7788da724dd880
nmarks99 <nmarks@anl.gov>
2024-05-08 15:07:15 -0500

    more configuration for real motors

--- iocBoot/ioc4idHHLM/cmds/AcsMotion.cmd
+++ iocBoot/ioc4idHHLM/cmds/AcsMotion.cmd
@@ -1,11 +1,15 @@
 # Use the following line if motorAcsMotion is built as a submodule of motor
 iocshLoad("$(MOTOR)/iocsh/ACS_Motion_tcp.iocsh", "INSTANCE=ACS1,IP_ADDR=164.54.115.62,NUM_AXES=8,IDLE_POLL=0.2")
 
-# Use the following line if motorAcsMotion is built as a standalone module
-# iocshLoad("$(MOTOR_ACSMOTION)/iocsh/ACS_Motion_tcp.iocsh", "INSTANCE=ACS1,IP_ADDR=10.0.0.100,NUM_AXES=8,IDLE_POLL=0.2")
-
 # Load motor records
 dbLoadTemplate("substitutions/AcsMotion.substitutions","P=$(PREFIX)")
 
 # Load an asyn record for debugging
 dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(PREFIX),R=asyn_1,PORT=ACS1_ETH,ADDR=0,OMAX=256,IMAX=256")
+
+# Set ERES to EFAC for each axis
+doAfterIocInit("dbpf('$(PREFIX)m1.ERES','0.00005')")
+doAfterIocInit("dbpf('$(PREFIX)m3.ERES','0.00005')")
+doAfterIocInit("dbpf('$(PREFIX)m4.ERES','0.00005')")
+doAfterIocInit("dbpf('$(PREFIX)m7.ERES','1.0')")
+doAfterIocInit("dbpf('$(PREFIX)m8.ERES','1.0')")
