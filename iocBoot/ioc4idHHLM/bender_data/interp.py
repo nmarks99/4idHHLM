7b25be5c0a37132b712812f9bc1da18f5b2f331c
nmarks99 <nmarks@anl.gov>
2024-06-10 15:44:22 -0500

    update bender interpolation, tested with sim motors

--- iocBoot/ioc4idHHLM/bender_data/interp.py
+++ iocBoot/ioc4idHHLM/bender_data/interp.py
@@ -8,7 +8,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.interpolate import interp1d
 
-N_POINTS = 100 # total number of interpolated values to generate
+N_POINTS = 200 # total number of interpolated values to generate
 SAVE = False   # save interpolated data to CSV
 
 # load the raw data and plot it
@@ -28,6 +28,15 @@ roc_concave = data[:,3][0:7]
 f_concave = interp1d(ucounts_concave, roc_concave, kind="cubic")
 x_fit_concave = np.linspace(ucounts_concave[0], ucounts_concave[-1], N_POINTS)
 y_fit_concave = [f_concave(xi) for xi in x_fit_concave]
+
+# Put original data points in interpolated data
+for i in range(len(ucounts_concave)):
+    c = ucounts_concave[i]
+    roc = roc_concave[i]
+    index = np.searchsorted(x_fit_concave, c)
+    x_fit_concave = np.insert(x_fit_concave, index, c)
+    y_fit_concave = np.insert(y_fit_concave, index, roc)
+
 fig1, ax1 = plt.subplots()
 ax1.plot(ucounts_concave, roc_concave, "bo", label="measured")
 ax1.plot(x_fit_concave, y_fit_concave, "r.", label="interpolated")
@@ -45,6 +54,15 @@ roc_convex = data[:,3][8:]
 f_convex = interp1d(ucounts_convex, roc_convex, kind="cubic")
 x_fit_convex = np.linspace(ucounts_convex[0], ucounts_convex[-1], N_POINTS)
 y_fit_convex = [f_convex(xi) for xi in x_fit_convex]
+
+for i in range(len(ucounts_convex)):
+    c = ucounts_convex[i]
+    roc = roc_convex[i]
+    index = np.searchsorted(x_fit_convex, c)
+    x_fit_convex = np.insert(x_fit_convex, index, c)
+    y_fit_convex = np.insert(y_fit_convex, index, roc)
+
+# plot
 fig2, ax2 = plt.subplots()
 ax2.plot(ucounts_convex, roc_convex, "bo", label="measured")
 ax2.plot(x_fit_convex, y_fit_convex, "r.", label="interpolated")
@@ -55,10 +73,15 @@ ax2.set_yticks(np.arange(-90,0,10))
 ax2.legend()
 ax2.grid()
 
+# put the (0,-407.09) point in the convex data
+index = np.searchsorted(x_fit_convex, 0.0)
+x_fit_convex = np.insert(x_fit_convex, index, 0.0)
+y_fit_convex = np.insert(y_fit_convex, index, -407.09)
+
 # save the interpolated data to CSV files
 if SAVE:
-    np.savetxt("concave.csv", np.column_stack([x_fit_concave, y_fit_concave]), delimiter=",")
-    np.savetxt("convex.csv", np.column_stack([x_fit_convex, y_fit_convex]), delimiter=",")
+    np.savetxt("concave_new.csv", np.column_stack([x_fit_concave, y_fit_concave]), delimiter=",")
+    np.savetxt("convex_new.csv", np.column_stack([x_fit_convex, y_fit_convex]), delimiter=",")
 
 # show the plots
 plt.show()
--- iocBoot/ioc4idHHLM/cmds/AcsMotion.cmd
+++ iocBoot/ioc4idHHLM/cmds/AcsMotion.cmd
@@ -8,8 +8,8 @@ dbLoadTemplate("substitutions/AcsMotion.substitutions","P=$(PREFIX)")
 dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(PREFIX),R=asyn_1,PORT=ACS1_ETH,ADDR=0,OMAX=256,IMAX=256")
 
 # Set ERES to EFAC for each axis
-doAfterIocInit("dbpf('$(PREFIX)m1.ERES','0.00005')")
-doAfterIocInit("dbpf('$(PREFIX)m3.ERES','0.00005')")
-doAfterIocInit("dbpf('$(PREFIX)m4.ERES','0.00005')")
-doAfterIocInit("dbpf('$(PREFIX)m7.ERES','1.0')")
-doAfterIocInit("dbpf('$(PREFIX)m8.ERES','1.0')")
+# doAfterIocInit("dbpf('$(PREFIX)m1.ERES','0.00005')")
+# doAfterIocInit("dbpf('$(PREFIX)m3.ERES','0.00005')")
+# doAfterIocInit("dbpf('$(PREFIX)m4.ERES','0.00005')")
+# doAfterIocInit("dbpf('$(PREFIX)m7.ERES','1.0')")
+# doAfterIocInit("dbpf('$(PREFIX)m8.ERES','1.0')")
